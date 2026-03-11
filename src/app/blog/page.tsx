da31bbff444fcb1afca74659c41bc246e71bcaeb
Md Tamim <tamimoli75285@gmail.com>
2026-03-11 12:50:45 +0600

    new change

--- src/app/blog/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function blogPage() {
-  return (
-    <div>blogPage</div>
-  )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-"use client"
-import { useRouter } from "next/navigation";
-
 
 export default function Home() {
-  const router = useRouter()
-  const goOutSite = ()=>{
-    router.push("/blog")
-  }
   return (
     <div>
-      <button onClick={goOutSite}>Go Desktop</button>
+      
     </div>
   );
 }
